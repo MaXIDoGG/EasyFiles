127a403012ac0e1a3b069295089407d602cd7cd4
MaXIDoGG <maks.nasonov.03@gmail.com>
2024-04-14 20:21:53 +0700

    add upload file

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,14 +1,43 @@
-import { Controller, HttpCode, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { FileElementResponse } from './dto/file-element.response';
+import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, MulterModule } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('files')
 export class FilesController {
 
-	@Post('upload')
-	@HttpCode(200)
-	@UseInterceptors(FileInterceptor('files'))
-	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
+	@Get("getFileById")
+	getFileById(@Param('id') id: string) {
 
 	}
+
+	@Post("uploadFile")
+	@UseInterceptors(FileInterceptor('file', {
+  storage: diskStorage({
+    destination: './uploads',
+		filename: (req, file, cb) => {
+      // Generating a 32 random chars long string
+      const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+      //Calling the callback passing the random name generated with the original extension name
+      cb(null, `${randomName}${extname(file.originalname)}`)
+    }
+  })
+}))
+	@ApiBody({
+		schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+	})
+	@ApiConsumes('multipart/form-data')
+	uploadFile(@UploadedFile() file: Express.Multer.File) {
+  	console.log(file);
+	}
+	
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { FileElementResponse } from './dto/file-element.response';
 
 @Injectable()
 export class FilesService {
-	async saveFiles(file: Express.Multer.File): Promise<FileElementResponse[]> {
-
+	uploadFile() {
+		
 	}
 }
